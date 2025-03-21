30b2285d89c9d210eb067d32824e1c5e0a6cccb0
Dave Dove <info@dovedesign.io>
2025-03-21 16:58:20 +0000

    navbar - mobile needs work

---
+++ src/components/Navigation.svelte
@@ -0,0 +1,77 @@
+<div class="container">
+  <nav>
+      <img src="/logo.svg" alt="Jadoo Logo">
+      <ul>
+          <li><a href="/">Destinations</a></li>
+          <li><a href="/">Hotels</a></li>
+          <li><a href="/">Flights</a></li>
+          <li><a href="/">Bookings</a></li>
+          <button class="button">Sign up</button>
+
+      </ul>
+  </nav>
+</div>
+
+<style>
+
+  .container {
+    width: 100%;
+    max-width: 1440px;
+    margin: 0 auto;
+  }
+
+  nav {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    background-color: #fff;
+    font-family: "Poppins", sans-serif;
+  }
+
+  img {
+    width: 100px;
+  }
+
+  ul {
+    display: flex;
+    align-items: center;
+    list-style: none;
+  }
+
+  li {
+    margin-right: 1.5rem;
+  }
+
+  a {
+    text-decoration: none;
+    color: #181e4b;
+    font-weight: 400;
+    transition: color 0.3s;
+  }
+
+  a:hover {
+    color: #df6951;
+  }
+
+  .button {
+    background-color: #df6951;
+    color: white;
+    padding: 12px 24px;
+    border-radius: 10px;
+    border: 2px solid #df6951;
+    font-weight: 600;
+    cursor: pointer;
+    text-align: center;
+  }
+
+  @media (max-width: 768px) {
+    nav {
+      flex-direction: column;
+      justify-content: center;
+    }
+
+    li {
+    margin-right: 1rem;
+  }
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,24 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+	import Navigation from '../components/Navigation.svelte';
+
+</script>
+
+<Navigation />
+<main>
+  <div class="container">
+    <h1>Page</h1>
+    <p>This is a page.</p>
+  </div>
+</main>
+
+<style>
+  main {
+    font-family: "Poppins", sans-serif;
+  }
+  .container {
+    width: 100%;
+    max-width: 1440px;
+    margin: 0 auto;
+  }
+</style>
+
